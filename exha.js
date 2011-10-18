9306c938625e07b64f359d2bd86b7934a83fe6c5
yuan.yudistira.B1 <yuanyudistiramail@gmail.com>
2011-10-18 16:34:55 +0700

    ok

---
+++ exha.js
@@ -0,0 +1,27 @@
+/*
+ Javascript Khusus Untuk Program Excellent Habit
+*/
+
+//Main Object
+var exha = new function() {
+    
+    //Info
+    this.programmer = "Yuan Yudistira";
+    this.getInfo = function () {
+        return this.programmer 
+    };
+    
+    //Set initial settings
+    this.init = function () {
+        this.displayCurrentDate("section#currentDateTime")
+    
+    };
+    
+    
+    //Utilities Function
+    this.displayCurrentDate = function(id) {
+        var currentDate = new Date();
+        $(id).replaceWith('Current Date: '+currentDate.getDate()+'/'+currentDate.getMonth()+'/'+currentDate.getFullYear());
+    }
+}
+
