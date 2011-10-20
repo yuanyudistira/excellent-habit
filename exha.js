3b5bffc6508c41fd2ba49c3b70f36275df500706
yuan.yudistira.B1 <yuanyudistiramail@gmail.com>
2011-10-20 18:27:06 +0700

    ok kamis malam

--- exha.js
+++ exha.js
@@ -2,6 +2,16 @@
  Javascript Khusus Untuk Program Excellent Habit
 */
 
+
+function onBodyLoad(){
+    //Add event listener deviceready (phonegap)
+    document.addEventListener("deviceready",exha.onDeviceReady,false);    
+    //initiate new javascript object
+    exha.init();
+
+} 
+
+
 //Main Object
 var exha = new function() {
     
@@ -14,13 +24,33 @@ var exha = new function() {
     //Set initial settings
     this.init = function () {
         
-        $(document).ready(function() {
-            //alert('Welcome to StarTrackr! Now no longer under police ...');
-        });
         this.displayCurrentDate("section#currentDateTime");
+         
+         $( document ).bind( "deviceready", function( event, data ){
+             //Lakukan sesuatu setelah device ready
+             alert("Device Ready");
+             
+          });
         
+        $( document ).bind( "pageload", function( event, data ){
+             //ini agar setiap kali page load, tetap dijalankan fungsi exha.init
+             exha.init();
+          });
+ 
+        this.onDeviceReady = function ()
+       {
+         //Code To access PhoneGap API CALL
+           alert("DEVICE READY");  
+       }
+    
+        //document.addEventListener("deviceready", yourCallbackFunction, false);
+        //Kita Matikan Ajax nya        
+        $(document).ready(function() {
+          // disable ajax nav
+          $.mobile.ajaxLinksEnabled = false;
+         });
+              
         //attach event to menu
-       
         $('ul#mainMenu li a').bind('click', function() {
             //exha.displayCurrentDate();
         });
@@ -31,7 +61,7 @@ var exha = new function() {
     //Utilities Function
     this.displayCurrentDate = function() {
         var currentDate = new Date();
-        $("section#currentDateTime").html('Today: '+currentDate.getDate()+'/'+currentDate.getMonth()+'/'+currentDate.getFullYear());
+        $("section#currentDateTime").text('Today: '+currentDate.getDate()+'/'+currentDate.getMonth()+'/'+currentDate.getFullYear());
 
     }
 }
